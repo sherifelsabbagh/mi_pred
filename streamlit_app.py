d0ac45611ebff7f702a333e7ef8e42128e092df6
Sherif Elsabbagh <60114574+sherifelsabbagh@users.noreply.github.com>
2024-11-29 17:22:49 +0000

    aaa

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,44 +26,34 @@ BMI = st.number_input("Body mass index (BMI)", min_value=0, max_value=500)
 Fever = st.selectbox("Fever", ["0", "1"])
 
 NV = st.selectbox("Nausea/Vomiting", ["0", "1"])
+Head = st.selectbox("Headache", ["0", "1"])
+Dia = st.selectbox("Diarrhea", ["0", "1"])
+Fatigue = st.selectbox("Fatigue", ["0", "1"])
+J = st.selectbox("Jaundice", ["0", "1"])
+EGP  =  st.selectbox("Epigastric pain", ["0", "1"])
 
-
-
-cp = st.selectbox("Chest Pain type", ["0", "1","2","	3"])
-
-trestbps = st.number_input("Resting Blood Pressure", min_value=0, max_value=500)
-
-chol= st.number_input("Serum Cholesterol in mg/dL ", min_value=0, max_value=500)
-
-fbs= st.selectbox("Fasting blood glucose > 120 mg/dL", ["1", "0"])
-
-restecg= st.selectbox("resting electrocardiographic results", ["0", "1","2"])
-
-thalach= st.number_input("Maximum heart rate by stress test", min_value=0, max_value=500)
-
-exang= st.selectbox("Exercise induced angina", ["0", "1"])
-
-oldpeak= st.number_input("ST depression", min_value=0, max_value=500)
-
-slope = st.selectbox("the slope of the peak exercise ST segment", ["0", "1","2"])
-
-ca= st.number_input("number of major vessels", min_value=0, max_value=3)
-
-thal = st.selectbox("Thalius test result", ["0", "1","2","3"])
+WBC = st.number_input("WBC count")
+RBC = st.number_input("RBC count")
+HGB = st.number_input("Hemoglobin (HGB)")
+Plat = st.number_input("Platelet count")
+AST = st.number_input("AST")
+ALT = st.number_input("ALT")
+RNA = st.number_input("RNA count")
+BHG = st.number_input("Baseline Histological Grading")
 
 # Prepare input data for the model
 input_data = np.array([[
-    age, gender,cp, trestbps ,chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal
+    age, gender,BMI, Fever ,NV, Head,Dia, Fatigue,J, EGP, WBC, RBC, HGB, Plat, AST, ALT, RNA, BHG
 ]])
 
 # Make prediction
-if st.button("Predict Heart Disease Risk"):
+if st.button("Predict Cirrhosis risk"):
     prediction = model.predict(input_data)
     prediction_proba = model.predict_proba(input_data)[0][1]  # Assuming model has predict_proba
 
     if prediction == 1:
-        st.write("### Result: High risk of Myocardial Infarction")
+        st.write("### Result: High risk of developing cirrhosis")
     else:
-        st.write("### Result: Low risk of Myocardial Infarction")
-    st.write(f"**Probability of Myocardial Infarction**: {prediction_proba * 100:.2f}%")
+        st.write("### Result: Low risk of developing cirrhosis")
+    st.write(f"**Probability of Cirrhosis**: {prediction_proba * 100:.2f}%")
 
